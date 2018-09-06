f6599e2dea1c7adf00a8d08f66cfdcfec6979ad2
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-06 07:59:51 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Menu
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int HoraInicio { get; set; }
+        public int HoraFinal { get; set; }
+        public Menu menu { get; set; }
+        public Platillos Platillos { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/platillos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Platillos
+    {
+        public string Identificador { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Ingredientes ingredientes { get; set; }
+        public float Precio
+        {
+            get
+            {
+                return Precio;
+                    
+            }
+    set
+            {
+                if (Precio < 50)
+                {
+                    Console.WriteLine("precio incorrecto");
+                }
+            
+            }
+        }
+
+
+    }
+}
+
